066b8f88b37964c70b5d9bca10dd93083d5d6de9
Corey Maylone <cmaylone@gmail.com>
2014-06-29 17:34:52 -0400

    Created function to send HTTP requests so it can be reused for other server implementations.

--- js/background.js
+++ js/background.js
@@ -15,18 +15,52 @@ chrome.contextMenus.create({
   onclick: handleTorrent
 });
 
-
 function handleTorrent(info, tab) {
   console.log(info);
 
   getServerUrl(function(serverUrl) {
-    var requestUrl = serverUrl.replace('&', '%26') + '/command/download';
+    var url = serverUrl.replace('&', '%26') + '/command/download';
+    var options = {
+      url: url,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: "urls="+info.linkUrl,
+      responseCallback: function(response) {
+        console.log("Response: %s", response);
+      }
+    };
 
-    var http = new XMLHttpRequest();
-    http.open('POST', requestUrl, true);
-    http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    http.send("urls="+info.linkUrl);
+    sendRequest(options);
   });
+}
+
+function sendRequest(options) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    // Ready state 4 means the request is finished and the response is ready.
+    if (xhr.readyState === 4) {
+      if(xhr.status === 200) {
+        options.responseCallback(xhr.responseText);
+      } else {
+        // TODO: Handle bad requests.
+        console.log("Error %s", xhr.statusText);
+      }
+
+    }
+  };
+
+  xhr.open(options.method, options.url, true);
+
+  if(options.headers) {
+    for(var header in options.headers) {
+      xhr.setRequestHeader(header, options.headers[header]);
+    }
+  }
+
+  xhr.send(options.data);
 }
 
 function getServerUrl(callback) {
